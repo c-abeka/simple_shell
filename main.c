9268a4a789a2faad69f690d24231a0ab324b0fcc
c-abeka <havelstech@gmail.com>
2022-11-17 22:58:56 +0300

    added a file

---
+++ main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * @ac: argument count
+ * @av: the variables
+ * Return: 0 on success
+ */
+int main(int ac, char *av[])
+{
+	int i = 0;
+
+	while(av[i])
+	{
+		printf("%s\n", av[i]);
+		i++;
+	}
+	return (0);
+}
