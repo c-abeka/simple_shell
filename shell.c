bbe384ee6ce17bfef9c4896c9d238de430087f36
c-abeka <havelstech@gmail.com>
2022-11-17 23:32:01 +0300

    simple shell prompt

--- shell.c
+++ shell.c
@@ -6,14 +6,17 @@
  * @av: arg variables
  * Return: 0 on success
  */
-int main(int ac, int *av)
+int main(int ac, char *av[])
 {
 	char *prompt = "#asus $";
 	char *linptr;
 	size_t n = 0;
 
+	(void)ac; (void)av;
+
 	printf("%s", prompt);
 	getline(&linptr, &n, stdin);
+	printf("%s\n", linptr);
 
 	free(linptr);
 
